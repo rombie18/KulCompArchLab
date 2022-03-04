5a9c229687ffaecbc2e88e2fd93bf5ce9fc18e56
Wout Rombouts <woutrombouts18@gmail.com>
2022-03-04 12:42:27 +0100

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -9,6 +9,41 @@
 
 int main(void) {
 
-  while (1) {
-  }
+	// CPU Frequentie = 48 MHz
+	// Systick interrupt elke 1 ms (1kHz)  --> 48000000 Hz / 1000 Hz --> Reload = 48000
+	SysTick_Config(48000);
+
+	// Interrupt aanzetten met een prioriteit van 128
+	NVIC_SetPriority(SysTick_IRQn, 128);
+	NVIC_EnableIRQ(SysTick_IRQn);
+
+	// Enable GPIOB and GPIOC blocks
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
+
+	// Enable button A
+	GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk;
+	// Enable button B
+	GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk;
+
+	// Enable pull-up resistors for buttons
+	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;
+	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
+
+	//
+
+	// Main loop
+	while (1) {
+
+	}
+}
+
+void Systick(void){
+	updateDisplay();
+}
+
+void updateDisplay() {
+
 }
