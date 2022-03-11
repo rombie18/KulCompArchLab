23716bd927df8b4189d5cb45ac022ec04bff077f
Wout Rombouts <woutrombouts18@gmail.com>
2022-03-11 12:20:33 +0100

    lghzrzif!èrgv

--- app/src/main.c
+++ app/src/main.c
@@ -1,49 +1,150 @@
-/**
- * @file main.c
- * @version 0.1
- *
- */
-
 #include <stdint.h>
 #include <stm32l4xx.h>
 
+char mux = 0;
+char display[4] = {0};
+
+
+void delay(unsigned int n){
+    volatile unsigned int delay = n;
+    while (delay--);
+}
+
+
+void multiplexer(){
+    if (mux == 1) {
+		GPIOA->ODR &= ~GPIO_ODR_OD8;             // 00 - 10 - 01 - 11
+		GPIOA->ODR &= ~GPIO_ODR_OD15;
+		GPIOA->ODR &= ~GPIO_ODR_OD6;
+		seg7(display[0]);
+    } else if (mux == 2) {
+    	GPIOA->ODR |= GPIO_ODR_OD8;             // 00 - 10 - 01 - 11
+		GPIOA->ODR &= ~GPIO_ODR_OD15;
+		GPIOA->ODR |= GPIO_ODR_OD6;
+		seg7(display[1]);
+    } else if (mux == 3) {
+    	GPIOA->ODR &= ~GPIO_ODR_OD8;             // 00 - 10 - 01 - 11
+		GPIOA->ODR |= GPIO_ODR_OD15;
+		GPIOA->ODR &= ~GPIO_ODR_OD6;
+		seg7(display[2]);
+    } else if (mux == 4) {
+    	GPIOA->ODR |= GPIO_ODR_OD8;             // 00 - 10 - 01 - 11
+		GPIOA->ODR |= GPIO_ODR_OD15;
+		GPIOA->ODR &= ~GPIO_ODR_OD6;
+		seg7(display[3]);
+		mux = 0;
+    }
+	mux++;
+}
+
+
+void seg7(char n){
+	//reset all segments
+    GPIOA->ODR &= ~(GPIO_ODR_OD7 | GPIO_ODR_OD5);
+    GPIOB->ODR &= ~(GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+
+    //segment  E
+    if (n == 0 || n == 2 || n == 6 || n == 8){
+        GPIOB->ODR |= GPIO_ODR_OD15;
+    }
+
+    //segment F
+    if (n != 1 && n != 3 && n != 7){
+        GPIOB->ODR |= GPIO_ODR_OD1;
+    }
+
+    //segment A
+    if (n != 1 && n != 4){
+        GPIOB->ODR |= GPIO_ODR_OD0;
+    }
+
+    //segment B
+    if (n != 5 && n != 6){
+        GPIOA->ODR |= GPIO_ODR_OD7;
+    }
+
+    //segment C
+    if (n != 2){
+        GPIOA->ODR |= GPIO_ODR_OD5;
+    }
+
+    //segment D
+    if (n != 1 && n != 4 && n != 7){
+        GPIOB->ODR |= GPIO_ODR_OD12;
+    }
+
+    //segment G
+    if (n != 1 && n != 7 && n != 0){
+        GPIOB->ODR |= GPIO_ODR_OD2;
+    }
+}
+
+
+void convertNumber(char* display, int number) {
+	char thousand = 0;
+	char hunderd = 0;
+	char ten = 0;
+	char one = 0;
+
+	thousand = number / 1000;
+	number -= thousand * 1000;
+	hunderd = number / 100;
+	number -= hunderd * 100;
+	ten = number / 10;
+	number -= ten * 10;
+	one = number;
+
+	display[0] = thousand;
+	display[1] = hunderd;
+	display[2] = ten;
+	display[3] = one;
+}
+
+void convertTime(char* display, int hours, int minutes) {
+	convertNumber(display, hours * 100 + minutes);
+}
+
+
 int main(void) {
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
 
-	// CPU Frequentie = 48 MHz
-	// Systick interrupt elke 1 ms (1kHz)  --> 48000000 Hz / 1000 Hz --> Reload = 48000
-	SysTick_Config(48000);
+    //7segmentjes
+    GPIOA->MODER &= ~(GPIO_MODER_MODE7_Msk | GPIO_MODER_MODE5_Msk);
+    GPIOA->MODER |= (GPIO_MODER_MODE7_0 | GPIO_MODER_MODE5_0);
+    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT7 | GPIO_OTYPER_OT5);
 
-	// Interrupt aanzetten met een prioriteit van 128
-	NVIC_SetPriority(SysTick_IRQn, 128);
-	NVIC_EnableIRQ(SysTick_IRQn);
+    GPIOB->MODER &= ~(GPIO_MODER_MODE0_Msk | GPIO_MODER_MODE12_Msk | GPIO_MODER_MODE15_Msk | GPIO_MODER_MODE1_Msk | GPIO_MODER_MODE2_Msk);
+    GPIOB->MODER |= (GPIO_MODER_MODE0_0 | GPIO_MODER_MODE12_0 | GPIO_MODER_MODE15_0 | GPIO_MODER_MODE1_0 | GPIO_MODER_MODE2_0);
+    GPIOB->OTYPER &= ~(GPIO_OTYPER_OT0 | GPIO_OTYPER_OT12 | GPIO_OTYPER_OT15 | GPIO_OTYPER_OT1 | GPIO_OTYPER_OT2);
 
-	// Enable GPIOB and GPIOC blocks
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
+    //mux + punt
+    GPIOA->MODER &= ~(GPIO_MODER_MODE8_Msk | GPIO_MODER_MODE15_Msk | GPIO_MODER_MODE6_Msk);
+    GPIOA->MODER |= (GPIO_MODER_MODE8_0 | GPIO_MODER_MODE15_0 | GPIO_MODER_MODE6_0);
+    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT8 | GPIO_OTYPER_OT15 | GPIO_OTYPER_OT6);
 
-	// Enable button A
-	GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk;
-	// Enable button B
-	GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk;
+    //knoppen
+    // GPIOB Pin 14 configureren als input
+    GPIOB->MODER &= ~(GPIO_MODER_MODE13_Msk | GPIO_MODER_MODE14_Msk);
+    GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPD13_Msk | GPIO_PUPDR_PUPD14_Msk);
+    GPIOB->PUPDR |= (GPIO_PUPDR_PUPD13_0 | GPIO_PUPDR_PUPD14_0);
 
-	// Enable pull-up resistors for buttons
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
 
-	//
+    //systick
+    SysTick_Config(48000);
 
-	// Main loop
-	while (1) {
+    //enable interupt
+    NVIC_SetPriority(SysTick_IRQn, 128);
+    NVIC_EnableIRQ(SysTick_IRQn);
 
-	}
+
+    convertTime(display, 11, 45);
+    while (1) {
+        multiplexer();
+    }
+
 }
 
-void Systick(void){
-	updateDisplay();
-}
-
-void updateDisplay() {
-
+void Systick_Handler(void) {
+	//convertTime(display, 11, 45);
 }
